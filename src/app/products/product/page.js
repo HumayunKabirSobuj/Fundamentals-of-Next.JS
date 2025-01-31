987bb2f51e037ca986cee1f725e672ef554b3217
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-01-31 20:03:11 +0600

    catch all route added

---
+++ src/app/products/product/page.js
@@ -0,0 +1,9 @@
+const SingleProductPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center">This is single product page</h1>
+    </div>
+  );
+};
+
+export default SingleProductPage;
